09375167d7fa415bfecacb4abdab066efe8d497b
Nilton Araripe <niltongoncalves571@gmail.com>
2026-09-12 10:40:00 -0300

    Adicionando classe principal e executura, primeiro molde do projeto feito

---
+++ ContaBancaria.java
@@ -0,0 +1,52 @@
+class Conta {
+    private String titular;
+    private int numero;
+    private double saldo;
+
+    public Conta(String titular, int numero){
+        this.titular = titular;
+        this.numero = numero;
+        this.saldo = 1000;
+    }
+    public String getTitular(){
+        return titular;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    public int getNumero(){
+        return numero;
+    }
+    public void deposito(double valor){
+        if (valor > 0){
+            this.saldo += valor;
+            System.out.println("Deposito no valor de: R$ " + valor 
+            + " Realizado com sucesso na conta de numero: " + getNumero() + " Titular: " + getTitular());
+            System.out.println("Saldo atual: R$ " + getSaldo());
+        } else {
+            System.out.println("O valor do deposito não pode ser negativo");
+        }
+    }
+    public void saque(double valor){
+        if (valor <= saldo && valor > 0){
+            this.saldo -= valor;
+            System.out.println("Saque no valor de: R$ " + valor + " realizado com sucesso");
+            System.out.println("Saldo atual: R$ " + getSaldo());
+        } else {
+            System.out.println("O valor do saque não pode exceder o saldo em conta e não pode ser negativo");
+            System.out.println("Saldo atual: R$ " + getSaldo());
+        }
+    }
+}
+public class ContaBancaria {
+    public static void main(String[] args){
+
+        Conta c1 = new Conta("Lorena", 1234);
+            System.out.println(c1.getSaldo());
+            c1.deposito(500);
+            c1.saque(200);
+            c1.saque(2000);
+            c1.deposito(-100);
+            System.out.println(c1.getSaldo());
+    }
+}
