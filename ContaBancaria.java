2442593ae81919567e7efd5328b68750fa6d852e
Nilton Araripe <niltongoncalves571@gmail.com>
2026-09-15 14:30:03 -0300

    Renomeando metodos e adicionando metodo exibir dados

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -17,16 +17,21 @@ class Conta {
     public int getNumero(){
         return numero;
     }
-    public void deposito(double valor){
+    public void exibirDados(){
+        System.out.println("Titular: %s" + getTitular()
+         + " | Numero da conta:" + getNumero()
+          + " | Saldo atual:" + getSaldo());
+    }
+    public void depositar(double valor){
         if (valor > 0){
             this.saldo += valor;
-            System.out.println("Deposito no valor de : R$ " + valor + "realizado com sucesso");
+            System.out.println("Deposito no valor de : R$ " + valor + " realizado com sucesso");
             System.out.println("Saldo atual: R$ " + getSaldo());
         } else {
-            System.out.println("O valor do deposito não pode ser negativo");
-        }
+            System.out.println("O valor do deposito deve ser maior que 0");
+        } 
     }
-    public void saque(double valor){
+    public void sacar(double valor){
         if (valor <= saldo && valor > 0){
             this.saldo -= valor;
             System.out.println("Saque no valor de: R$ " + valor + " realizado com sucesso");
@@ -42,10 +47,10 @@ public class ContaBancaria {
 
         Conta c1 = new Conta("Lorena", 1234);
             System.out.println(c1.getSaldo());
-            c1.deposito(500);
-            c1.saque(200);
-            c1.saque(2000);
-            c1.deposito(-100);
-            System.out.println("Conta numero: " + c1.getNumero() + " Titular: " + c1.getTitular() + ", Saldo atual: " + c1.getSaldo());
+            c1.depositar(500);
+            c1.sacar(200);
+            c1.sacar(2000);
+            c1.depositar(-100);
+            c1.exibirDados();
     }
 }
