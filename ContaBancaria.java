6ed7705bc3a52207ed49fdc3b508a3df84c80659
Nilton Araripe <niltongoncalves571@gmail.com>
2026-09-15 14:56:19 -0300

    Adicionando segunda conta

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -3,36 +3,42 @@ class Conta {
     private int numero;
     private double saldo;
 
-    public Conta(String titular, int numero){
+    public Conta(String titular, int numero) {
         this.titular = titular;
         this.numero = numero;
         this.saldo = 1000;
     }
-    public String getTitular(){
+
+    public String getTitular() {
         return titular;
     }
-    public double getSaldo(){
+
+    public double getSaldo() {
         return saldo;
     }
-    public int getNumero(){
+
+    public int getNumero() {
         return numero;
     }
-    public void exibirDados(){
-        System.out.println("Titular: %s" + getTitular()
-         + " | Numero da conta:" + getNumero()
-          + " | Saldo atual:" + getSaldo());
+
+    public void exibirDados() {
+        System.out.println("Titular: " + getTitular()
+                + " | Numero da conta:" + getNumero()
+                + " | Saldo atual:" + getSaldo());
     }
-    public void depositar(double valor){
-        if (valor > 0){
+
+    public void depositar(double valor) {
+        if (valor > 0) {
             this.saldo += valor;
             System.out.println("Deposito no valor de : R$ " + valor + " realizado com sucesso");
             System.out.println("Saldo atual: R$ " + getSaldo());
         } else {
             System.out.println("O valor do deposito deve ser maior que 0");
-        } 
+        }
     }
-    public void sacar(double valor){
-        if (valor <= saldo && valor > 0){
+
+    public void sacar(double valor) {
+        if (valor <= saldo && valor > 0) {
             this.saldo -= valor;
             System.out.println("Saque no valor de: R$ " + valor + " realizado com sucesso");
             System.out.println("Saldo atual: R$ " + getSaldo());
@@ -42,15 +48,18 @@ class Conta {
         }
     }
 }
-public class ContaBancaria {
-    public static void main(String[] args){
 
-        Conta c1 = new Conta("Lorena", 1234);
-            System.out.println(c1.getSaldo());
-            c1.depositar(500);
-            c1.sacar(200);
-            c1.sacar(2000);
-            c1.depositar(-100);
-            c1.exibirDados();
+public class ContaBancaria {
+    public static void main(String[] args) {
+
+        Conta c1 = new Conta("Lorena", 1504);
+        Conta c2 = new Conta("Nilton", 2011);
+        System.out.println(c1.getSaldo());
+        c1.exibirDados();
+        c2.exibirDados();
+        c2.depositar(200);
+        c1.sacar(100);
+        c1.exibirDados();
+        c2.exibirDados();
     }
 }
